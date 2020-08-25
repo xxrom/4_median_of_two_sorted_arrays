61c67d8f2e703f1f6158d9777f453cadec9371aa
Chernyshov_Nikita <xpom5566@mail.ru>
2020-08-25 21:58:41 +0300

    solved: 100ms (69.2%) / 14.2mb (9.4%) not so good

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
+from typing import List
+
+class Solution:
+  def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+    ans = 0
+    merged = nums1[:] + nums2[:]
+    print(merged)
+
+    merged.sort()
+    print(merged)
+
+    if len(merged) < 2:
+      if len(merged) == 0:
+        return ans
+
+      return merged[0]
+
+
+    middle = (len(merged) - 1) // 2
+
+    if len(merged) % 2 == 0:
+      ans = (merged[middle] + merged[middle + 1]) / 2
+    else:
+      ans = merged[middle]
+
+    return ans
+
+
 my = Solution()
-n = 0
-trueAns=0
-ans = my.X(n)
+n0 = [2,1]
+n1 = [4,3]
+trueAns=2.5
+
+ans = my.findMedianSortedArrays(n0, n1)
+
 print("ans", ans, ans == trueAns)
+
+# Runtime: 100 ms, faster than 69.22% of Python3 online submissions for Median of Two Sorted Arrays.
+# Memory Usage: 14.2 MB, less than 9.36% of Python3 online submissions for Median of Two Sorted Arrays.
