af5fc80e1f7ab9d2f970db06fa3c998da44a900d
Chernyshov_Nikita <xpom5566@mail.ru>
2020-08-25 22:32:44 +0300

    solved: 100ms (69.2%) / 13.8 mb (99.3%) memory O(N)/ time O(N*Lg(N))[quick sort could take a lot of time ...]

--- main.py
+++ main.py
@@ -1,34 +1,65 @@
 from typing import List
 
 class Solution:
+  def fillDict(self, d: dict, nums: List[int]):
+    for num in nums:
+      if num not in d:
+        d[num] = 1
+      else:
+        d[num] = d[num] + 1
+
   def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
     ans = 0
-    merged = nums1[:] + nums2[:]
-    print(merged)
 
-    merged.sort()
-    print(merged)
+    dick = {}
+    size = len(nums1) + len(nums2)
+    middle = size // 2
 
-    if len(merged) < 2:
-      if len(merged) == 0:
-        return ans
+    self.fillDict(dick, nums1)
+    self.fillDict(dick, nums2)
 
-      return merged[0]
+    print(dick)
 
+    keys = list(dick.keys())
+    keys.sort()
 
-    middle = (len(merged) - 1) // 2
+    # print(keys)
+    count = 0
 
-    if len(merged) % 2 == 0:
-      ans = (merged[middle] + merged[middle + 1]) / 2
+    one = None
+    two = None
+
+    for key in keys:
+      print(key)
+      count = count + dick[key]
+
+      if count >= middle:
+        if one == None:
+          # print('one %d' % key)
+          one = key
+
+      if count > middle:
+        if two == None:
+          # print('two %d' % key)
+          two = key
+          break
+
+    if one == None:
+      one = 0
+    if two == None:
+      two = 0
+
+    if size % 2 == 0:
+      ans = (one + two) / 2
     else:
-      ans = merged[middle]
+      ans = two
 
     return ans
 
 
 my = Solution()
 n0 = [2,1]
-n1 = [4,3]
+n1 = [3,4]
 trueAns=2.5
 
 ans = my.findMedianSortedArrays(n0, n1)
@@ -36,4 +67,4 @@ ans = my.findMedianSortedArrays(n0, n1)
 print("ans", ans, ans == trueAns)
 
 # Runtime: 100 ms, faster than 69.22% of Python3 online submissions for Median of Two Sorted Arrays.
-# Memory Usage: 14.2 MB, less than 9.36% of Python3 online submissions for Median of Two Sorted Arrays.
+# Memory Usage: 13.8 MB, less than 99.25% of Python3 online submissions for Median of Two Sorted Arrays.
